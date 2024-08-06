83f7c7f90f64656bd8e08f4d8ffa87f8a10f22c5
Umme Zaiba <83930541+Umme-Zaiba@users.noreply.github.com>
2024-08-06 21:59:42 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import ToggleText from './components/useState/ToggleText';
+
+function App() {
+  return (
+    <div className="App">
+      <ToggleText/>
+    </div>
+  );
+}
+
+export default App;
